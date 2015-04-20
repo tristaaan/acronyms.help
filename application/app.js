b98d322e53bba58a85f8bbdadaff42da66c6c3f7
Tristan <thrawst88@gmail.com>
2015-04-20 14:11:24 -0600

    commented out database code, data read from file - this project isn't large enough to make a database necessary

--- application/app.js
+++ application/app.js
@@ -1,18 +1,23 @@
+var fs = require('fs');
+var path = require('path');
 var express = require('express');
-var database = require('./database');
+//var database = require('./database');
 var limitedArray = require('./limitedArray');
 
 var app = express();
-var limArray = new limitedArray(8, '_id');
+var limArray = new limitedArray(8, 'acronym');
 
-var host = process.env.HOST;
-var dbport = process.env.DBPORT;
-var dbname = process.env.DBNAME;
-var username = process.env.USERNAME;
-var pass = process.env.PASSWORD;
-var connection = new database(host, dbport, dbname, username, pass);
+// discontinued database, see commented block below ~line 47
+// var host = process.env.HOST;
+// var dbport = process.env.DBPORT;
+// var dbname = process.env.DBNAME;
+// var username = process.env.USERNAME;
+// var pass = process.env.PASSWORD;
+// var connection = new database(host, dbport, dbname, username, pass);
 
-app.use(express.static(__dirname + '/../public'));
+var src = [];
+
+app.use('/', express.static(path.join(__dirname, '../public')));
 
 app.get('/', function(req, res){
   res.sendFile('index.html');
@@ -25,22 +30,58 @@ app.get('/fetch/:expr', function(req, res){
     res.send([]);
   }
   else{
-    connection.find({acronym: new RegExp('^'+expression, 'i')}, function(error, data){
-      if(error){
-        console.log(error);
-      }
-      else{
-        res.send(data);
-        if (data.length > 0){
-          limArray.push(data[Math.floor(Math.random()*data.length)]);
+    if (src.length == 0){
+      fs.readFile('./sampledata/data.json', function(err, data) {
+        if (err){
+          console.log(err);
+          return;
         }
-      }
-    });
+        src = JSON.parse(data);
+        sendData(res, expression);
+      });
+    }
+    else{
+      sendData(res, expression);
+    }
+    // if you have a database uncommend this and others, 
+    // for now reading the file should be ok
+    // connection.find({acronym: new RegExp('^'+expression, 'i')}, function(error, data){
+    //   if(error){
+    //     console.log(error);
+    //   }
+    //   else{
+    //     res.send(data);
+    //     if (data.length > 0){
+    //       limArray.push(data[Math.floor(Math.random()*data.length)]);
+    //     }
+    //   }
+    // });
   }
 });
 
 app.get('/recents', function(req, res){
   res.send(limArray.contents);
 });
+
+function sendData(res, expression){
+  var list = pick(src, new RegExp('^'+expression, 'i'));
+  res.setHeader('Content-Type', 'application/json');
+  res.send(list);
+  if (list.length > 0){
+    limArray.push(randomIndex(list));
+  }
+}
+
+function randomIndex(arr){
+  return arr[Math.floor(Math.random()*arr.length)];
+}
+
+function pick (src, regexp) {
+  return src.filter(function(el){
+    if (regexp.test(el.acronym)) {
+      return el;
+    }
+  });
+}
 
 module.exports = app;
